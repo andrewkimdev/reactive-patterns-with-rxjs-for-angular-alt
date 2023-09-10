1437b29aa14611077f7338b5f3c07a8e72ec9742
Andrew KIM <andrewbkim.dev@gmail.com>
2023-09-10 17:53:37 +0900

    add error handling for http call

--- recipes-book-front/src/app/core/services/recipes.service.ts
+++ recipes-book-front/src/app/core/services/recipes.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError, of } from 'rxjs';
+
 import { Recipe } from '../model/recipe.model';
 import { environment } from 'src/environments/environment';
+
 const BASE_PATH = environment.basePath
 
 @Injectable({
@@ -9,8 +12,8 @@ const BASE_PATH = environment.basePath
 })
 
 export class RecipesService {
-
-  recipes$ = this.http.get<Recipe[]>(`${BASE_PATH}/recipes`);
+  recipes$ = this.http.get<Recipe[]>(`${BASE_PATH}/recipes`).pipe(
+    catchError(error => of([])));
 
   constructor(private http: HttpClient) { }
 }
